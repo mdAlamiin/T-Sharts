7487846123a149b8db979f95c06962a7c88a2443
unknown <031alamin@gmail.com>
2023-04-09 03:25:46 +0600

    toast add

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Tshirt from '../Tshirt/Tshirt';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Tshirt from "../Tshirt/Tshirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
+import toast from 'react-hot-toast';
 
 const Home = () => {
-    const tshirts =useLoaderData();
+  const tshirts = useLoaderData();
+  //   const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    return (
-        <div>
-            <h2>This is Home File: {tshirts.length}</h2>
-            {
-                tshirts.map(tshirt => 
-                (<Tshirt
-                key={tshirt._id}
-                tshirt={tshirt}
-                ></Tshirt>)
-                )
-            }
-        </div>
-    );
+  const handleAddToCart = (tshirt) => {
+    const exists = cart.find(ts => ts._id === tshirt._id);
+    if (exists) {
+        toast('You have already addedT-Shirt');
+    } 
+    else {
+        const newCart = [...cart, tshirt];
+        setCart(newCart);
+    }
+    
+  };
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter(ts => ts._id !== id);
+    setCart(remaining);
+    
+  };
+
+  return (
+    <div className="home-container">
+      <div className="T-shirt-container">
+        {tshirts.map((tshirt) => (
+          <Tshirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          ></Tshirt>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/Components/Tshirt/Tshirt.jsx
+++ src/Components/Tshirt/Tshirt.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Tshirt.css';
 
-const Tshirt = ({tshirt}) => {
-    const {picture}= tshirt
+const Tshirt = ({tshirt, handleAddToCart}) => {
+    const {picture, price, name}= tshirt
     return (
-        <div>
-            {/* <h1>This Tshirt page</h1> */}
-            
-                <img src={picture} alt="" />
-            
+        <div className='T-shirt'>
+            <img src={picture} alt="" />
+            <h4>{name}</h4>
+            <p>Price: ${price}</p>
+            <button onClick={()=>handleAddToCart(tshirt)}>Buy Now</button>
         </div>
     );
 };
